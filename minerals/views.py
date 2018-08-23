5027035a4be17fa8996390b605627994cdd4b073
Khem Myrick <khem.myrick@gmail.com>
2018-08-22 20:32:29 -0700

    Added basic mineral list view
    str manipulation ftw.

--- minerals/views.py
+++ minerals/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Mineral
 
 def index(request):
     return HttpResponse("Welcome to the Minerals index.")
+
+def mineral_list(request):
+    minerals = Mineral.objects.all()
+    output = ''
+    for mineral in minerals:
+        output += str(mineral)
+        output += ', '
+    # output = ', '.join(minerals)
+    return HttpResponse(output)
